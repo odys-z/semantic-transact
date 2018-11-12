f0a8f823f42b1033824adf5c159ee76ca9189923
odysz <odysseusj@163.com>
2018-11-13 00:24:00 +0800

    merge antlr4

--- semantic.transact/src/main/java/io/odysz/semantics/sql/parts/Sql.java
+++ semantic.transact/src/main/java/io/odysz/semantics/sql/parts/Sql.java
@@ -2,14 +2,25 @@ package io.odysz.semantics.sql.parts;
 
 
 import io.odysz.semantics.sql.parts.Logic.op;
+import io.odysz.semantics.sql.parts.antlr.ExprsVisitor;
 import io.odysz.semantics.sql.parts.condition.Condit;
 
 public class Sql {
 	
-	public static Condit condt(String format, String... args) {
+	public static Condit condt(String format, Object... args) {
 		// return new Condt(new ExprBuilder(format, (Object[])args));
+
 		// TODO parse
-		return new Condit(Logic.op(format), args[0], args[1]);
+		String exp = String.format(format, args);
+		
+		return parseCondit(exp);
+
+		// return new Condit(Logic.op(format), args[0], args[1]);
+	}
+
+	private static Condit parseCondit(String exp) {
+		ExprsVisitor expr = ExprsVisitor.parse(exp);
+		return new Condit(expr.op(), expr.lop(), expr.rop());
 	}
 
 	public static Condit condt(op op, String loperand, String roperand) {
--- semantic.transact/src/main/java/io/odysz/semantics/sql/parts/antlr/ExprsVisitor.java
+++ semantic.transact/src/main/java/io/odysz/semantics/sql/parts/antlr/ExprsVisitor.java
@@ -1,7 +1,43 @@
 package io.odysz.semantics.sql.parts.antlr;
 
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+
+import gen.antlr.sql.exprs.SearchExprs;
+import gen.antlr.sql.exprs.SearchExprs.Search_conditionContext;
 import gen.antlr.sql.exprs.SearchExprsBaseVisitor;
+import gen.antlr.sql.exprs.TSqlLexer;
+import io.odysz.semantics.sql.parts.Logic;
 
 public class ExprsVisitor extends SearchExprsBaseVisitor<String> {
+	public static ExprsVisitor parse(String strExpr) {
+		ANTLRInputStream inputStream = new ANTLRInputStream(strExpr);
+//		ANTLRInputStream inputStream = new ANTLRInputStream(
+//	            "I would like to [b][i]emphasize[/i][/b] this and [u]underline [b]that[/b][/u] ." +
+//	            "Let's not forget to quote: [quote author=\"John\"]You're wrong![/quote]");
+	        TSqlLexer markupLexer = new TSqlLexer(inputStream);
+	        CommonTokenStream commonTokenStream = new CommonTokenStream(markupLexer);
+	        SearchExprs markupParser = new SearchExprs(commonTokenStream);
+	 
+	        Search_conditionContext ctx = markupParser.search_condition();                
+	        ExprsVisitor visitor = new ExprsVisitor();                
+	        visitor.visit(ctx);  
+	        return visitor;
+	}
+	
+	public Logic.op op() {
+		return null;
+		
+	}
+
+	public String rop() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String lop() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
