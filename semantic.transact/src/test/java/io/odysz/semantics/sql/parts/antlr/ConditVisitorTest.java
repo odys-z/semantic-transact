03187fae359f23e88fc819aa3e8c791ef463adf5
odysz <odysseusj@163.com>
2018-11-20 00:17:52 +0800

    a.id = b.roleId AND (a.c > d OR x > y) AND a.c = '5'

--- semantic.transact/src/test/java/io/odysz/semantics/sql/parts/antlr/ConditVisitorTest.java
+++ semantic.transact/src/test/java/io/odysz/semantics/sql/parts/antlr/ConditVisitorTest.java
@@ -13,8 +13,8 @@ public class ConditVisitorTest {
 		// String strExpr = "f.funcId = rf.funcId and rf.roleId = 'r001'";
 		// String strExpr = "F.FUNCID = RF.FUNCID AND RF.ROLEID = 'r001'";
 
-		String strExpr = "a.id = b.roleId and (a.c>d OR x> y)";
-		String expect = "a.id = b.roleId AND (a.c > d OR x > y)";
+		String strExpr = "a.id = b.roleId and (a.c>d OR x> y) and a.c = '5'";
+		String expect = "a.id = b.roleId AND (a.c > d OR x > y) AND a.c = '5'";
 		Condit condt = ConditVisitor.parse(strExpr);
 		String sql = condt.sql();
 		System.out.print(sql);
