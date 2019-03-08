4543aa49a78942d0b4c7982916b56fe35f974036
odys zhou <odysseusj@163.com>
2019-03-08 13:55:31 +0800

    milestone: jclient.jave got menu tree

--- semantic.transact/src/main/java/io/odysz/common/LangExt.java
+++ semantic.transact/src/main/java/io/odysz/common/LangExt.java
@@ -1,5 +1,8 @@
 package io.odysz.common;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class LangExt {
 	/**Split and trim elements.
 	 * <p>Empty element won't be ignored if there are 2 consequent separator. <br>
@@ -19,5 +22,20 @@ public class LangExt {
 				regex = "\\s*" + regex + "\\s*";
 			return s.split(regex);
 		}
+	}
+	
+	public static String toString(String[] ss) {
+		return ss == null ? null : Arrays.stream(ss).collect(Collectors.joining(",", "[", "]"));
+	}
+	
+	/**Convert 2D array to string: "[{ss[0][1]: ss[0][1]}, {ss[1][0]: ss[1][1]}, ...]"
+	 * @param ss
+	 * @return
+	 */
+	public static String toString(String[][] ss) {
+		return Arrays.stream(ss)
+				.filter(s -> s != null)
+				.map(e -> toString(e))
+				.collect(Collectors.joining(",", "[", "]"));
 	}
 }
