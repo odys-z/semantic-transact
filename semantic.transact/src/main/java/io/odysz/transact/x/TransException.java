e10df08369b85031783e4d949e30c34720197674
odys zhou <odysseusj@163.com>
2019-06-19 22:10:03 +0800

    0

--- semantic.transact/src/main/java/io/odysz/transact/x/TransException.java
+++ semantic.transact/src/main/java/io/odysz/transact/x/TransException.java
@@ -1,11 +1,13 @@
 package io.odysz.transact.x;
 
+import io.odysz.common.LangExt;
+
 public class TransException extends Exception {
 	private static final long serialVersionUID = 1L;
 
 
 	public TransException(String format, Object... args) {
-		super(String.format(format, args));
+		super(LangExt.isblank(format) ? null : String.format(format, args));
 	}
 	
 }
