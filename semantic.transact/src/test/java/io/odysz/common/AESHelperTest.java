496d9ecc04e777bb07ea5df99ac04c506e5911e2
Odys Zhou <odys.zhou@gmail.com>
2021-04-26 10:49:11 +0800

    AES Tested

---
+++ semantic.transact/src/test/java/io/odysz/common/AESHelperTest.java
@@ -0,0 +1,36 @@
+package io.odysz.common;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+import org.junit.Test;
+
+public class AESHelperTest {
+
+	/**C# Debug Trace:<pre>
+	Check this at server side:
+	Cypher:
+	4VGdDR9qJlq36bQGI+Sx3A==
+	Key:
+	io.github.odys-z
+	IV:
+	DITVJZA2mSDAw496hBz6BA==
+	Expacting:
+	Plain Text</pre>
+	 * @throws IOException 
+	 * @throws GeneralSecurityException 
+	 * 
+	 */
+	@Test
+	public void testDecrypt() throws GeneralSecurityException, IOException {
+		String cipher = "4VGdDR9qJlq36bQGI+Sx3A==";
+		String key = "io.github.odys-z";
+		String iv = "DITVJZA2mSDAw496hBz6BA==";
+		String plain = AESHelper.decrypt(cipher, key,
+				AESHelper.decode64(iv));
+		assertEquals("Plain Text", plain.trim());
+	}
+
+}
