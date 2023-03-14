5e830f9b69090a8a9d0fffc549c175fe8f48125f
odys zhou <odysseusj@163.com>
2023-03-14 19:12:59 +0800

    add comments

--- semantic.transact/src/main/java/io/odysz/transact/sql/PageInf.java
+++ semantic.transact/src/main/java/io/odysz/transact/sql/PageInf.java
@@ -4,6 +4,25 @@ import java.util.ArrayList;
 
 import io.odysz.anson.Anson;
 
+/**
+ * <p>DB records' page information.</p>
+ * 
+ * <p>This type should presented in all request with A = records in the near future.</p>
+ * 
+ * <h6>Use case:</h6>
+ * 1. Semantic.jserv/io.oz.spreadsheet.SpreadsheetReq.pageInf,<br>
+ *    Anclient.ts:<br>
+ *    curriculum/views/north/AllDecissionsComp.conds<br>
+ *    curriculum/views/north/MyComp.conds<br>
+ *    ...<br>
+ *    
+ * 2. Docsync.jserv/io.oz.jserv.dbsync.DBSyncReq.pageInf,<br>
+ *    Anclient.java:<br>
+ *    Docsync.jserv/io.oz.jserv.dbsync.DBWorker#syncTabl()<br>
+ * 3. ...
+ * @author odys-z@github.com
+ *
+ */
 public class PageInf extends Anson {
 
 	public long page;
