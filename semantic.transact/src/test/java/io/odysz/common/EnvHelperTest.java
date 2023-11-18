aa4fa1e7cd61189b6584ef179c126ef1b8c1a0c3
Odys Zhou <odys.zhou@gmail.com>
2023-11-18 16:39:25 -0500

    fix paths test

--- semantic.transact/src/test/java/io/odysz/common/EnvHelperTest.java
+++ semantic.transact/src/test/java/io/odysz/common/EnvHelperTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.lang.reflect.Field;
 import java.util.Map;
 
+import org.apache.commons.io_odysz.FilenameUtilsTest;
 import org.junit.jupiter.api.Test;
 
 public class EnvHelperTest {
@@ -19,10 +20,10 @@ public class EnvHelperTest {
 			home = "~";
 			System.setProperty("HOME", home);
 		}
-		assertEquals(home + "/v", EnvPath.replaceEnv("$HOME/v"));
+		FilenameUtilsTest.assertPathEquals(home + "/v", EnvPath.replaceEnv("$HOME/v"));
 		
 		System.setProperty("VOLUME", "volume");
-		assertEquals("volume/v", EnvPath.replaceEnv("$VOLUME/v"));
+		FilenameUtilsTest.assertPathEquals("volume/v", EnvPath.replaceEnv("$VOLUME/v"));
 	}
 
 	@Test
@@ -34,34 +35,24 @@ public class EnvHelperTest {
 
 		String encoded = EnvPath.encodeUri(extroot, "ody", "000001 f.txt");
 		
-		assertPathEquals("$VOLUME_HOME/shares/ody/000001 f.txt", encoded);
+		FilenameUtilsTest.assertPathEquals("$VOLUME_HOME/shares/ody/000001 f.txt", encoded);
 
 		String abspath = EnvPath.decodeUri("", encoded);
-		assertPathEquals("/home/ody/volume/shares/ody/000001 f.txt", abspath);
+		FilenameUtilsTest.assertPathEquals("/home/ody/volume/shares/ody/000001 f.txt", abspath);
 		
 		args = "upload,uri,userId,cate,docName".split(",");
 		encoded = EnvPath.encodeUri(extroot, "admin", "000002 f.txt");
-		assertPathEquals("$VOLUME_HOME/shares/admin/000002 f.txt", encoded);
+		FilenameUtilsTest.assertPathEquals("$VOLUME_HOME/shares/admin/000002 f.txt", encoded);
 
 		abspath = EnvPath.decodeUri(rtroot, encoded);
-		// assertEquals("/home/ody/upload/admin/000003 f.txt", abspath);
-		assertPathEquals("/home/ody/volume/shares/admin/000002 f.txt", abspath);
+		FilenameUtilsTest.assertPathEquals("/home/ody/volume/shares/admin/000002 f.txt", abspath);
 		
 		// Override
 		System.setProperty("VOLUME_HOME", "/home/alice/vol");
 		abspath = EnvPath.decodeUri(rtroot, encoded);
-		assertPathEquals("/home/alice/vol/shares/admin/000002 f.txt", abspath);
+		FilenameUtilsTest.assertPathEquals("/home/alice/vol/shares/admin/000002 f.txt", abspath);
 	}
 	
-	private void assertPathEquals(String expect, String actual) {
-		try {
-			assertEquals(expect, actual);
-		} catch (AssertionError e) {
-			assertEquals(expect.replaceAll("/", "\\"), actual);
-		}
-		
-	}
-
 	/**Only Linux/MacOs
 	 * https://stackoverflow.com/a/40682052/7362888
 	 * @param newenv
--- semantic.transact/src/test/java/org/apache/commons/io_odysz/FilenameUtilsTest.java
+++ semantic.transact/src/test/java/org/apache/commons/io_odysz/FilenameUtilsTest.java
@@ -9,39 +9,48 @@ public class FilenameUtilsTest {
 
 	@Test
 	public void testConcatStringStringArray() {
+//		try {
+	     assertPathEquals("/foo/bar", concat("/foo/", "bar"));
+	     assertPathEquals("/foo/bar", concat("/foo", "bar"));
+	     assertPathEquals("/bar", concat("/foo", "/bar"));
+	     assertPathEquals("C:/bar", concat("/foo", "C:/bar"));
+	     assertPathEquals("C:bar", concat("/foo", "C:bar"));
+	     assertPathEquals("/foo/bar", concat("/foo/a/", "../bar"));
+	     assertPathEquals(null, concat("/foo/", "../../bar"));
+	     assertPathEquals("/bar", concat("/foo/", "/bar"));
+	     assertPathEquals("/bar", concat("/foo/..", "/bar"));
+	     assertPathEquals("/foo/bar/c.txt", concat("/foo", "bar/c.txt"));
+	     assertPathEquals("/foo/c.txt/bar", concat("/foo/c.txt", "bar"));
+	     
+	     assertPathEquals("/git/semantic-jserv/docsync.jserv/src/test/res/WEB-INF",
+	    	   concat("/git/semantic-jserv/docsync.jserv", "./src/test/res/WEB-INF"));
+//		}
+//		catch (AssertionError e) {
+//	     assertEquals("\\foo\\bar", concat("/foo/", "bar"));
+//	     assertEquals("\\foo\\bar", concat("/foo", "bar"));
+//	     assertEquals("\\bar", concat("/foo", "/bar"));
+//	     assertEquals("C:\\bar", concat("/foo", "C:/bar"));
+//	     assertEquals("C:bar", concat("/gfoo", "C:bar"));
+//	     assertEquals("\\foo\\bar", concat("/foo/a/", "../bar"));
+//	     assertEquals(null, concat("\\foo/", "../../bar"));
+//	     assertEquals("\\bar", concat("/foo/", "/bar"));
+//	     assertEquals("\\bar", concat("/foo/..", "/bar"));
+//	     assertEquals("\\foo\\bar\\c.txt", concat("/foo", "bar/c.txt"));
+//	     assertEquals("\\foo\\c.txt\\bar", concat("/foo/c.txt", "bar"));
+//	     
+//	     assertEquals("\\git\\semantic-jserv\\docsync.jserv\\src\\test\\res\\WEB-INF",
+//	    	   concat("/git/semantic-jserv/docsync.jserv", "./src/test/res/WEB-INF"));
+//			
+//		}
+	}
+
+	public static void assertPathEquals(String expect, String actual) {
 		try {
-	     assertEquals("/foo/bar", concat("/foo/", "bar"));
-	     assertEquals("/foo/bar", concat("/foo", "bar"));
-	     assertEquals("/bar", concat("/foo", "/bar"));
-	     assertEquals("C:/bar", concat("/foo", "C:/bar"));
-	     assertEquals("C:bar", concat("/foo", "C:bar"));
-	     assertEquals("/foo/bar", concat("/foo/a/", "../bar"));
-	     assertEquals(null, concat("/foo/", "../../bar"));
-	     assertEquals("/bar", concat("/foo/", "/bar"));
-	     assertEquals("/bar", concat("/foo/..", "/bar"));
-	     assertEquals("/foo/bar/c.txt", concat("/foo", "bar/c.txt"));
-	     assertEquals("/foo/c.txt/bar", concat("/foo/c.txt", "bar"));
-	     
-	     assertEquals("/git/semantic-jserv/docsync.jserv/src/test/res/WEB-INF",
-	    	   concat("/git/semantic-jserv/docsync.jserv", "./src/test/res/WEB-INF"));
+			assertEquals(expect, actual);
+		} catch (AssertionError e) {
+			assertEquals(expect.replaceAll("/", "\\\\"), actual);
 		}
-		catch (AssertionError e) {
-	     assertEquals("\\foo\\bar", concat("/foo/", "bar"));
-	     assertEquals("\\foo\\bar", concat("/foo", "bar"));
-	     assertEquals("\\bar", concat("/foo", "/bar"));
-	     assertEquals("C:\\bar", concat("/foo", "C:/bar"));
-	     assertEquals("C:bar", concat("/gfoo", "C:bar"));
-	     assertEquals("\\foo\\bar", concat("/foo/a/", "../bar"));
-	     assertEquals(null, concat("\\foo/", "../../bar"));
-	     assertEquals("\\bar", concat("/foo/", "/bar"));
-	     assertEquals("\\bar", concat("/foo/..", "/bar"));
-	     assertEquals("\\foo\\bar\\c.txt", concat("/foo", "bar/c.txt"));
-	     assertEquals("\\foo\\c.txt\\bar", concat("/foo/c.txt", "bar"));
-	     
-	     assertEquals("\\git\\semantic-jserv\\docsync.jserv\\src\\test\\res\\WEB-INF",
-	    	   concat("/git/semantic-jserv/docsync.jserv", "./src/test/res/WEB-INF"));
-			
-		}
+		
 	}
 
 }
