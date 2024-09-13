0a721c6df19ceca3cb0cd0991cd7182f9f4d538f
ody <ody@Infochange.ca>
2024-09-13 20:11:20 +0800

    experiment: buffered/queued sqlite commits

--- semantic.transact/src/main/java/io/odysz/common/dbtype.java
+++ semantic.transact/src/main/java/io/odysz/common/dbtype.java
@@ -1,6 +1,6 @@
 package io.odysz.common;
 
-public enum dbtype {mysql(0), ms2k(1), oracle(2), sqlite(3), postGIS(4);
+public enum dbtype {mysql(0), ms2k(1), oracle(2), sqlite(3), postGIS(4), sqlite_queue(5);
 	private final int value;
 	private dbtype(int value) { this.value = value; }
 	public int getValue() { return value; }
