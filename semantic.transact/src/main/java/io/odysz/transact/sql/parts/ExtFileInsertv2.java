4e771cea0cbddc1cb24fe31263bba2f111b2f837
Ody Z <odys.zhou@gmail.com>
2025-07-15 18:31:33 +0800

    clean

--- semantic.transact/src/main/java/io/odysz/transact/sql/parts/ExtFileInsertv2.java
+++ semantic.transact/src/main/java/io/odysz/transact/sql/parts/ExtFileInsertv2.java
@@ -48,26 +48,16 @@ public class ExtFileInsertv2 extends AbsPart {
 	 */
 
 	/**
-	 * Create an exteranl file representation by setting the absolute root path.
+	 * Create an exteranl file representation by setting the root path.
 	 * This path is used to access file together with the relative path set
 	 * by {@link ExtFileInsert#prefixPath(String, String...)}.<br>
 	 * 
-	 * The argument doesn't have to be absolute path if the runtime can access a file from a relative path.<br>
+	 * The argument doesn't have to be absolute path if the runtime can access a file from a relative path.
 	 * 
-	 * But servlet containers needing absolute paths to access file, so this must been set to the absolute path,
-	 * such as the return of <a href='https://docs.oracle.com/javaee/6/api/javax/servlet/ServletContext.html'>
-	 * javax.servlet.ServletContext#getRealPath(String)</a>.<br>
-	 * 
-	 * @param file id to be resolved as prefix of filename
-	 * @param volume e. g. args[0] in semantics.xml, $VOLUME_HOME
-	 * @param stx instance of run time context
 	 * @throws TransException 
-	public ExtFileInsertv2(String volume, Resulving fn, ISemantext stx) throws TransException {
-		this(volume, (ExprPart)fn, stx);
-	}
 	 */
 	public ExtFileInsertv2(ExtFilePaths extpaths) throws TransException {
-		this.extpaths = extpaths; //new ExtFilePaths(volume, resulvingId.sql(ctx), null);
+		this.extpaths = extpaths;
 	}
 
 	public ExtFileInsertv2 filename(String name) {
